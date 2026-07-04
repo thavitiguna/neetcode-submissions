11971bd1034c80c3e5138ec18dc7b31f5b595318
thavitiguna <43792437+thavitiguna@users.noreply.github.com>
2026-07-04 07:59:44 +0530

    Add: replace-elements-with-greatest-element-on-right-side - submission-0

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-0.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] ReplaceElements(int[] arr) {
+        int max=int.MinValue;
+        for(int i=0;i<arr.Length;i++)
+        {
+            for(int j=i+1;j<arr.Length;j++)
+            {
+                if(arr[j]>max)
+                {
+                    max = arr[j];
+                }
+
+            }
+            arr[i]=max;
+            max=0;
+            
+        }
+        arr[arr.Length-1] =-1;
+        return arr;
+        
+    }
+}
