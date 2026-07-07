f0ac311d931c9ac43e2c4b02a3f2c76cc3762700
thavitiguna <43792437+thavitiguna@users.noreply.github.com>
2026-07-07 22:52:02 +0530

    Add: score-of-a-string - submission-1

---
+++ Data Structures & Algorithms/score-of-a-string/submission-1.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int ScoreOfString(string s) {
+        int sum = 0;
+        for (int i=1;i<s.Length;i++)
+        {
+              int temp = (int)s[i-1] - (int)s[i];
+              sum = sum + Math.Abs(temp);
+              // sum += Math.Abs(s[i-1] - s[i]);
+        }
+        return sum;
+        
+    }
+}
